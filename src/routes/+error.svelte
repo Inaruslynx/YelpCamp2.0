9cb09249a39b47372869ea4d11037cdf0371477a
Inaruslynx <inaruslynx@gmail.com>
2022-09-20 21:50:10 +0000

    Created Error route

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { page } from '$app/stores';
+  </script>
+
+<div class="row">
+    <div class="col-6 offset-3">
+        <div class="alert alert-danger" role="alert">
+            <h4 class="alert-heading">{$page.status}</h4>
+            {#if (process.env.NODE_ENV !== 'production' && $page.error)} 
+            <p>{$page.error.message}</p>
+            {/if}
+          </div>
+    </div>
+  </div>
