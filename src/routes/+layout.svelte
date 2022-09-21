73d8b72b29d53285232717d01490bee2e1feea3b
Inaruslynx <inaruslynx@gmail.com>
2022-09-21 00:20:18 +0000

    Added Modal and moved it around

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,30 @@
+<script lang="ts">
+	import Modal from './modal.svelte';
+
+	let showModal: boolean = false;
+	let toggleModal = () => {
+		showModal = !showModal;
+	};
+</script>
+
+<Modal message="Hello, Ninjas!" {showModal} on:click={toggleModal} />
 <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/login">Login</a>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/login">Login</a>
+    <button on:click|once={toggleModal}>Open Modal</button>
 </nav>
 
-<slot></slot>
+<slot />
 
 <footer>
-    <p>Author: Joshua Edwards</p>
-    <p>&copy;2022</p>
+	<p>Author: Joshua Edwards</p>
+	<p>&copy;2022</p>
 </footer>
 
 <style>
-    footer{
-        text-align: center;
-        vertical-align: bottom;
-    }
+	footer {
+		text-align: center;
+		vertical-align: bottom;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,47 @@
-<h1><strong>Welcome to SvelteKit</strong></h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Modal from './modal.svelte';
+
+	let showModal: boolean = false;
+	let toggleModal = () => {
+		showModal = !showModal;
+	};
+	let people = [
+		{ name: 'yoshi', beltColor: 'black', age: 25, id: 1 },
+		{ name: 'mario', beltColor: 'orange', age: 45, id: 2 },
+		{ name: 'luigi', beltColor: 'brown', age: 35, id: 3 }
+	];
+	const handleClick = (e: MouseEvent, id: number) => {
+		people = people.filter((person) => person.id != id);
+		console.log(e);
+	};
+</script>
+
+<main>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			{#if person.beltColor === 'black'}
+				<p><strong>MASTER NINJA</strong></p>
+			{/if}
+			<p>{person.age} years old, {person.beltColor} belt.</p>
+			<button on:click={(e) => handleClick(e, person.id)}>delete</button>
+		</div>
+	{:else}
+		<p>There are no people to show...</p>
+	{/each}
+</main>
 
 <style>
-	h1 {
+	main {
 		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
 	}
-	p {
-		text-align: center;
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
 	}
 </style>
